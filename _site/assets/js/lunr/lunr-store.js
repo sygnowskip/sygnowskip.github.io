072e3db5741d4bf6a7ce55469b270e5bb6847bed
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-10-24 22:21:14 +0200

    feature: welcome post changes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
+        "title": "Welcome!",
         "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": ["blog"],
         "tags": ["jekyll","welcome"],
-        "url": "http://localhost:4000/blog/welcome-to-jekyll/",
+        "url": "http://localhost:4000/blog/welcome/",
         "teaser":null}]
