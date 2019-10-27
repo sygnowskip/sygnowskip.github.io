9aa61f8ce12617482516c66484c41d238af52cde
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-10-27 13:03:33 +0100

    feature: NUnit logger

---
+++ sources/common-components/Common/Common.Tests/LoggingBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Common.Tests
+{
+    public static class LoggingBuilderExtensions
+    {
+        public static ILoggingBuilder AddNUnitConsoleOutput(this ILoggingBuilder logging)
+        {
+            logging.AddProvider(new NUnitLoggerProvider());
+            return logging;
+        }
+    }
+}
---
+++ sources/common-components/Common/Common.Tests/NUnitLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace Common.Tests
+{
+    public class NUnitLogger : ILogger
+    {
+        private readonly string _name;
+
+        public NUnitLogger(string name)
+        {
+            _name = name;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            TestContext.Progress.WriteLine($"{logLevel,10}|{eventId,30}|{_name,80}|{formatter(state, exception)}");
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
