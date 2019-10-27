0f2224f7ed1d4416608eb6b24b662ff703eb533f
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-10-27 13:03:49 +0100

    feature: common configuration for Docker

---
+++ sources/common-components/Common/Common.Docker/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Common.Docker.Resources;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Docker
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDockerSetup(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<MsSqlResourceAwaiter>();
+            return serviceCollection;
+        }
+    }
+}
