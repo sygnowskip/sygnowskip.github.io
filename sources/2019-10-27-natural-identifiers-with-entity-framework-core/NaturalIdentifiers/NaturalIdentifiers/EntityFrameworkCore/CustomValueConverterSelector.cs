7b4cb013d8feb54abebc4e9c5a5ecd8064ed26fd
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-10-28 21:28:28 +0100

    feature: extensions for EF Core mechanisms to handle natural identifiers

---
+++ sources/2019-10-27-natural-identifiers-with-entity-framework-core/NaturalIdentifiers/NaturalIdentifiers/EntityFrameworkCore/CustomValueConverterSelector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace NaturalIdentifiers.EntityFrameworkCore
+{
+    public class CustomValueConverterSelector : ValueConverterSelector
+    {
+        private readonly ConcurrentDictionary<(Type ModelClrType, Type ProviderClrType), ValueConverterInfo> _converters
+            = new ConcurrentDictionary<(Type ModelClrType, Type ProviderClrType), ValueConverterInfo>();
+
+        public CustomValueConverterSelector(ValueConverterSelectorDependencies dependencies)
+            : base(dependencies)
+        {
+        }
+
+        public override IEnumerable<ValueConverterInfo> Select(Type modelClrType, Type providerClrType = null)
+        {
+            var baseConverters = base.Select(modelClrType, providerClrType);
+            foreach (var converter in baseConverters)
+            {
+                yield return converter;
+            }
+
+            var underlyingModelType = UnwrapNullableType(modelClrType);
+            var underlyingProviderType = UnwrapNullableType(providerClrType);
+
+            if (underlyingProviderType == null || underlyingProviderType == typeof(Guid))
+            {
+                var isIdentifier = typeof(Identifier).IsAssignableFrom(underlyingModelType);
+                if (isIdentifier)
+                {
+                    var converterType = typeof(IdentifierValueConverter<>).MakeGenericType(underlyingModelType);
+
+                    yield return _converters.GetOrAdd((underlyingModelType, typeof(Guid)), _ =>
+                    {
+                        return new ValueConverterInfo(
+                            modelClrType: modelClrType,
+                            providerClrType: typeof(Guid),
+                            factory: valueConverterInfo => (ValueConverter)Activator.CreateInstance(converterType, valueConverterInfo.MappingHints));
+                    });
+                }
+            }
+        }
+
+        private static Type UnwrapNullableType(Type type) => type == null ? null : Nullable.GetUnderlyingType(type) ?? type;
+    }
+}
---
+++ sources/2019-10-27-natural-identifiers-with-entity-framework-core/NaturalIdentifiers/NaturalIdentifiers/EntityFrameworkCore/IdentifierValueConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace NaturalIdentifiers.EntityFrameworkCore
+{
+    public class IdentifierValueConverter<TIdentifier> : ValueConverter<TIdentifier, Guid>
+        where TIdentifier : Identifier
+    {
+        public IdentifierValueConverter(ConverterMappingHints mappingHints = null)
+            : base(id => id.Value, value => Create(value), mappingHints)
+        {
+        }
+
+        private static TIdentifier Create(Guid id) => Activator.CreateInstance(typeof(TIdentifier), id) as TIdentifier;
+    }
+}
