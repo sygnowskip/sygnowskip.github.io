ba29963710fd1e9c5626578630c0d15324ae6f68
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-10-28 21:27:57 +0100

    feature: persistence for Customer

---
+++ sources/2019-10-27-natural-identifiers-with-entity-framework-core/NaturalIdentifiers/NaturalIdentifiers/Persistence/CustomerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NaturalIdentifiers.Persistence
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable(nameof(Customer));
+
+            builder.HasKey(customer => customer.Id);
+
+            builder.Property(customer => customer.FirstName);
+            builder.Property(customer => customer.LastName);
+
+            builder.OwnsOne(customer => customer.Address);
+        }
+    }
+}
