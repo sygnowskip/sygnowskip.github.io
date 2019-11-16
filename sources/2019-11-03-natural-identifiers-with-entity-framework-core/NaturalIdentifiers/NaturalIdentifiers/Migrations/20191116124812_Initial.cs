1d301db3fe43f7441a9d0fdb9f1eca9eabe8fe9b
Paweł Sygnowski <sygnowski.p@gmail.com>
2019-11-16 13:56:08 +0100

    chore: example migration

---
+++ sources/2019-11-03-natural-identifiers-with-entity-framework-core/NaturalIdentifiers/NaturalIdentifiers/Migrations/20191116124812_Initial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NaturalIdentifiers.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Address_Street = table.Column<string>(nullable: true),
+                    Address_PostalCode = table.Column<string>(nullable: true),
+                    Address_City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    Customer_FirstName = table.Column<string>(nullable: true),
+                    Customer_LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
